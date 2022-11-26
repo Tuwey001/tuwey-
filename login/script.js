59d6f59ce3e5d517029b45d1f9673b8ec02e535c
Tuwey001 <118714451+Tuwey001@users.noreply.github.com>
2022-11-26 17:38:40 +0300

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+console.log("this is my first program in external javascript")
+var davis='I like doing javascript'
+console.log(davis)
+const name =`davisasfdf`
+console.log(name)
+/*coments makes the codes more easyly to read 
+and understand*/
+//numbers
+const firstno = 2;
+const secnumb =3;
+const result= firstno-secnumb
+console.log(result)
